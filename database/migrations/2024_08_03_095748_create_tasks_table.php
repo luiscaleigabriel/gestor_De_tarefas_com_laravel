458365a4f2cddc338686187437edf70951b965c7
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2024-08-03 11:11:07 +0100

    criei o bd da aplicação

--- database/migrations/2024_08_03_095748_create_tasks_table.php
+++ database/migrations/2024_08_03_095748_create_tasks_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_admin')->default(false);
-            $table->rememberToken();
+            $table->text('description');
+            $table->time('bigin');
+            $table->time('done');
+            $table->set('status', [0, 1, 2]);
+            $table->unsignedBigInteger('id_anexo');
+            $table->foreign('id_anexo')->references('id')->on('anexos')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
